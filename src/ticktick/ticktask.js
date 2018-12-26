3ad0693c2c682fec2b9c3ad6156637c164f28fee
kaisoz <kaisoz85@gmail.com>
2018-12-27 00:58:08 +0100

    Update timezone and date adjustment

--- src/ticktick/ticktask.js
+++ src/ticktick/ticktask.js
@@ -1,14 +1,21 @@
 var ObjectID = require("bson-objectid");
+var moment = require('moment');
+
+function adjustDate(date) 
+{
+    return moment(date).toISOString().replace("Z", "+0000");
+}
 
 class TickTask
 {
     constructor(data)
     {
         if(!data.title){
-            throw "Invalid task data"; // This needs to be updated to grab dynamically
+            throw "Invalid task data";
         }
 
         this.title        = data.title;
+        this.modifiedTime = adjustDate(data.modifiedTime);
         this.assignee     = (data.assignee)     ? data.assignee     : null;
         this.content      = (data.content)      ? data.content      : "";
         this.deleted      = (data.deleted)      ? data.deleted      : 0;
@@ -18,7 +25,6 @@ class TickTask
         this.isDirty      = (data.isDirty)      ? data.isDirty      : true;
         this.items        = (data.items)        ? data.items        : [];
         this.local        = (data.local)        ? data.local        : true;
-        this.modifiedTime = (data.modifiedTime) ? data.modifiedTime : new Date().toISOString().replace("Z", "+0000"); //"2017-08-12T17=04=51.982+0000";
         this.priority     = (data.priority)     ? data.priority     : 0;
         this.progress     = (data.progress)     ? data.progress     : 0;
         this.projectId    = (data.projectId)    ? data.projectId    : 0;
@@ -30,7 +36,7 @@ class TickTask
         this.startDate    = (data.startDate)    ? data.startDate    : null;
         this.status       = (data.status)       ? data.status       : 0;
         this.tags         = (data.tags)         ? data.tags         : [];
-        this.timeZone     = (data.timeZone)     ? data.timeZone     : "America/New_York"; // This needs to be updated to grab dynamically
+        this.timeZone     = (data.timeZone)     ? data.timeZone     : "Europe/Madrid";
     }
 }
 
