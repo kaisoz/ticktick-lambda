78503cd942dca19faeba84e344193fd7e445137a
kaisoz <kaisoz85@gmail.com>
2018-12-27 02:04:29 +0100

    Take timezone into account when creating task dates

--- src/ticktick/ticktask.js
+++ src/ticktick/ticktask.js
@@ -1,9 +1,10 @@
 var ObjectID = require("bson-objectid");
 var moment = require('moment');
+require('moment-timezone')
 
-function adjustDate(date) 
+function adjustDate(date, timezone) 
 {
-    return moment(date).toISOString().replace("Z", "+0000");
+    return moment.tz(date, timezone).toISOString().replace("Z", "+0000");
 }
 
 class TickTask
@@ -15,28 +16,29 @@ class TickTask
         }
 
         this.title        = data.title;
-        this.modifiedTime = adjustDate(data.modifiedTime);
-        this.assignee     = (data.assignee)     ? data.assignee     : null;
-        this.content      = (data.content)      ? data.content      : "";
-        this.deleted      = (data.deleted)      ? data.deleted      : 0;
-        this.dueDate      = (data.dueDate)      ? data.dueDate      : null;
-        this.id           = (data.id)           ? data.id           : ObjectID();
-        this.isAllDay     = (data.isAllDay)     ? data.isAllDay     : null;
-        this.isDirty      = (data.isDirty)      ? data.isDirty      : true;
-        this.items        = (data.items)        ? data.items        : [];
-        this.local        = (data.local)        ? data.local        : true;
-        this.priority     = (data.priority)     ? data.priority     : 0;
-        this.progress     = (data.progress)     ? data.progress     : 0;
-        this.projectId    = (data.projectId)    ? data.projectId    : 0;
-        this.reminder     = (data.reminder)     ? data.reminder     : null;
-        this.reminders    = (data.reminders)    ? data.reminders    : [{ id: ObjectID(), trigger: "TRIGGER=PT0S"}];
-        this.remindTime   = (data.remindTime)   ? data.remindTime   : null;
-        this.repeatFlag   = (data.repeatFlag)   ? data.repeatFlag   : null;
-        this.sortOrder    = (data.sortOrder)    ? data.sortOrder    : 0;
-        this.startDate    = (data.startDate)    ? data.startDate    : null;
-        this.status       = (data.status)       ? data.status       : 0;
-        this.tags         = (data.tags)         ? data.tags         : [];
-        this.timeZone     = (data.timeZone)     ? data.timeZone     : "Europe/Madrid";
+        this.timeZone     = (data.timeZone)     ? data.timeZone                 : "Europe/Madrid";
+        this.assignee     = (data.assignee)     ? data.assignee                 : null;
+        this.content      = (data.content)      ? data.content                  : "";
+        this.deleted      = (data.deleted)      ? data.deleted                  : 0;
+        this.id           = (data.id)           ? data.id                       : ObjectID();
+        this.isAllDay     = (data.isAllDay)     ? data.isAllDay                 : null;
+        this.isDirty      = (data.isDirty)      ? data.isDirty                  : true;
+        this.items        = (data.items)        ? data.items                    : [];
+        this.local        = (data.local)        ? data.local                    : true;
+        this.priority     = (data.priority)     ? data.priority                 : 0;
+        this.progress     = (data.progress)     ? data.progress                 : 0;
+        this.projectId    = (data.projectId)    ? data.projectId                : 0;
+        this.reminder     = (data.reminder)     ? data.reminder                 : null;
+        this.reminders    = (data.reminders)    ? data.reminders                : [{ id: ObjectID(), trigger: "TRIGGER=PT0S"}];
+        this.remindTime   = (data.remindTime)   ? data.remindTime               : null;
+        this.repeatFlag   = (data.repeatFlag)   ? data.repeatFlag               : null;
+        this.sortOrder    = (data.sortOrder)    ? data.sortOrder                : 0;
+        this.status       = (data.status)       ? data.status                   : 0;
+        this.tags         = (data.tags)         ? data.tags                     : [];
+
+        this.modifiedTime = adjustDate(data.modifiedTime, this.timeZone);
+        this.dueDate      = (data.dueDate)      ? adjustDate(data.dueDate, this.timeZone)      : null;
+        this.startDate    = (data.startDate)    ? adjustDate(data.startDate, this.timeZone)    : null;
     }
 }
 
